74436ac3b5e54428ca7cdcfa47d88aa14e6576bf
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-29 17:34:38 +0200

    end snack bonus

--- js-snack-bonus/script.js
+++ js-snack-bonus/script.js
@@ -17,5 +17,14 @@ se è dispari inseriscilo nell’array.
 const numberList = [];
 
 //chiedere per 6 volte un numero con prompt
+for (let i = 0; i < 6; i++) {
+    const userNumber = Number(prompt(`Inserisci un numero`))
+    console.log(userNumber);
 
-//qualsiasi numero dispari inserire nell'array altrimenti adios numero pari!
+    //qualsiasi numero dispari inserire nell'array altrimenti adios numero pari!
+    if (userNumber % 2 !== 0) {
+        numberList.push(userNumber);
+    } 
+}
+//stampare in console
+console.log(`Ecco i numeri dispari che hai digitato: ${numberList}`);
