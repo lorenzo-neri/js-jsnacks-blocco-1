451260cfcd1bb32d70915d61828666923a7b84d8
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-29 16:43:20 +0200

    end third snack

---
+++ js-snack-3/script.js
@@ -0,0 +1,30 @@
+/* 
+
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+# tools
+- prompt
+- const/let
+- console.log()
+- document.querySelector
+- document.createElement
+- .append
+*/
+
+let total = 0
+
+//chiedere per 10 volte di inserire un numero con prompt
+for (let i = 0; i < 10; i++) {
+    const userNumbers = Number(prompt(`${i + 1}) Inserisci un numero`));
+    console.log(userNumbers);
+
+    total += userNumbers
+}
+//stampare la somma di tutti i numeri inseriti
+if (!isNaN(total)) {
+    console.log(`La somma dei numeri digitati è ${total}`);
+} else {
+    alert('I CARATTERI DIGITATI NON SONO NUMERICI, SI PREGA DI RIPROVARE!');
+    location.reload();
+}
