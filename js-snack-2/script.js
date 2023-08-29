9ca1dc33668969f4f00de20db4bcd453b3e50f2d
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-29 15:21:32 +0200

    start second snack

---
+++ js-snack-2/script.js
@@ -0,0 +1,23 @@
+/* 
+
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+
+# tools:
+- prompt
+- const/let
+- console.log()
+- .length
+- document.querySelector
+- document.createElement
+- .append
+
+*/
+
+//Chiedere la prima parola all'utente con prompt
+
+//Chiedere la seconda parola all'utente con prompt
+
+//stampare prima la parola più corta
+
+//stampare dopo la parola più lunga
