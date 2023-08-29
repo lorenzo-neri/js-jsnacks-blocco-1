96e6500f37da7d05fc03cc7cf310be001fdfd230
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-29 15:10:32 +0200

    end first snack + personal bonus

--- js-snack-1/script.js
+++ js-snack-1/script.js
@@ -4,7 +4,40 @@ L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
 
 # tools:
-
-
-
+- prompt
+- const/let
+- console.log()
+- .length
+- document.querySelector
+- document.createElement
+- .append
 */
+
+//BONUS PERSONALE
+//creare degli elementi nel DOM
+const bodyEl = document.querySelector('body');
+
+const longerWordEl = document.createElement('div');
+
+bodyEl.append(longerWordEl);
+
+//aggiungere gli elementi al DOM
+
+//stampare gli elmenti in pagina
+
+//Chiedere la prima parola all'utente con prompt
+const firstWord = prompt('Ciao, scrivi una prima parola');
+console.log(firstWord);
+
+//Chiedere la seconda parola all'utente con prompt
+const secondWord = prompt('Ora scrivi una seconda parola');
+console.log(secondWord);
+
+//stampare in console la parola più lunga
+if(firstWord.length > secondWord.length){
+    console.log(firstWord);
+    longerWordEl.append(`La parola più lunga da te scritta è: ${firstWord}`);
+} else {
+    console.log(secondWord);
+    longerWordEl.append(`La parola più lunga da te scritta è: ${secondWord}`);
+}
